5b218fa61a766e1cfb4c838e1db6c49de8d111cc
arpitdm <arpitdm@gmail.com>
2025-02-09 17:16:23 +0000

    update: prints all data objects after processing

--- analysis/create_in_memory_dataset_blob.py
+++ analysis/create_in_memory_dataset_blob.py
@@ -4,6 +4,7 @@ This script creates an InMemoryDatasetBlob for use in training GNN-style models.
 Usage:
 >>> python analysis/create_in_memory_dataset_blob.py --config_file config/in_memory_dataset_blob/sss2-1b_1p.yaml
 """
+import scanpy as sc
 
 from vqniche.dataloaders.in_memory_dataset_blob import InMemoryDatasetBlob
 from vqniche.utils.config_parsers import parse_arguments, collect_configs
@@ -22,7 +23,7 @@ def main(config: dict):
     pre_filter = config['dataset']['pre_filter']
     overwrite = config['dataset']['overwrite']
 
-    dataset = InMemoryDatasetBlob(
+    dataset_blob = InMemoryDatasetBlob(
                     name=dataset_name,
                     feature_names=feature_names,
                     label_names=label_names,
@@ -32,8 +33,13 @@ def main(config: dict):
                     pre_filter=pre_filter,
                     overwrite=overwrite
                 )
-
-    print(f"Processed data saved at {dataset.processed_dir}")
+    
+    for data_batch in dataset_blob:
+        print(f"Batch: {data_batch.batch}")
+        print(f"Data: {data_batch}")
+        print("")
+    
+    print(f"Processed data saved at {dataset_blob.processed_dir}")
 
 
 if __name__ == '__main__':
