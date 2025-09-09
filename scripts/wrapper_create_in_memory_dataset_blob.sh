22920eeaa7ad4797760efe1f92f604ad2885b649
arpitdm <arpitdm@gmail.com>
2025-09-10 00:09:43 +0100

    silver to gold for kidney data

--- scripts/wrapper_create_in_memory_dataset_blob.sh
+++ scripts/wrapper_create_in_memory_dataset_blob.sh
@@ -33,6 +33,13 @@ case $DATASET_NAME in
         CORES=8
         QUEUE="gpu-lotfollahi"
         ;;
+    xhk1020-CV1-CV2-5b_1p)
+        RAM="40G"
+        TIME="1:00"
+        NUM_GPUS=1
+        CORES=8
+        QUEUE="gpu-lotfollahi"
+        ;;
     *)
         echo "Invalid dataset name"
         exit 1
